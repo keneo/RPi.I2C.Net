841209d80b32946ec6f9e0af7277c8b962012b87
keneo <bartek@szabat.com>
2013-09-29 12:34:40 +0200

    make SampleApp simple again (move advanced sample to a new app)

---
+++ SampleApp.Mpu6050/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RPi.I2C.Net;
+using RPi.I2C.Net.Devices.Mpu6050Driver;
+
+namespace SampleApp.Mpu6050
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sleep = args.Length == 0 ? 0 : int.Parse(args[0]);
+
+            using (var bus = I2CBus.Open("/dev/i2c-1"))
+            {
+                var sensor = new RPi.I2C.Net.Devices.Mpu6050Driver.Mpu6050(bus);
+
+                sensor.Init();
+
+                for (; ; )
+                {
+                    int probes = 100;
+
+                    Readings[] accumulator = new Readings[probes];
+
+                    for (int i = 0; i < probes; i++)
+                    {
+                        accumulator[i] = sensor.ReadRawReadings().Decode();
+
+                        if (sleep > 0)
+                            System.Threading.Thread.Sleep(sleep);
+                    }
+
+                    Readings r = new Readings()
+                    {
+                        Acc = new double[3] { accumulator.Average(a => a.Acc[0]), accumulator.Average(a => a.Acc[1]), accumulator.Average(a => a.Acc[2]), },
+                        Temp = accumulator.Average(a => a.Temp),
+                        Gyro = new double[3] { accumulator.Average(a => a.Gyro[0]), accumulator.Average(a => a.Gyro[1]), accumulator.Average(a => a.Gyro[2]), }
+                    };
+
+                    //double temp = sensor.ReadTemperature();
+
+
+
+                    DateTime now = DateTime.Now;
+
+                    Console.WriteLine(now + "." + now.Millisecond.ToString("000") + ": " + r.ToString());
+                    //Console.WriteLine(t);
+
+                }
+            }
+        }
+    }
+}
--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using RPi.I2C.Net;
-using RPi.I2C.Net.Devices.Mpu6050Driver;
 
 
 namespace SampleApp
@@ -12,45 +10,12 @@ namespace SampleApp
 	{
 		static void Main(string[] args)
 		{
-		    int sleep = args.Length == 0 ? 0 : int.Parse(args[0]);
-
-			using (var bus = I2CBus.Open("/dev/i2c-1"))
+            using (var bus = RPi.I2C.Net.I2CBus.Open("/dev/i2c-1"))
 			{
-                var sensor = new Mpu6050(bus);
+                var dev = bus[42];
 
-			    sensor.Init();
-
-                for (; ; )
-                {
-                    int probes = 100;
-
-                    Readings[] accumulator = new Readings[probes];
-
-                    for (int i = 0; i < probes; i++)
-                    {
-                        accumulator[i] = sensor.ReadRawReadings().Decode();
-
-                        if (sleep > 0)
-                            System.Threading.Thread.Sleep(sleep);
-                    }
-
-                    Readings r = new Readings()
-                                     {
-                                         Acc = new double[3]{accumulator.Average(a=>a.Acc[0]),accumulator.Average(a=>a.Acc[1]),accumulator.Average(a=>a.Acc[2]),},
-                                         Temp = accumulator.Average(a=>a.Temp),
-                                         Gyro = new double[3]{accumulator.Average(a=>a.Gyro[0]),accumulator.Average(a=>a.Gyro[1]),accumulator.Average(a=>a.Gyro[2]),}
-                                     };
-
-                    //double temp = sensor.ReadTemperature();
-
-
-
-                    DateTime now = DateTime.Now;
-
-                    Console.WriteLine(now + "." + now.Millisecond.ToString("000") + ": " + r.ToString());
-                    //Console.WriteLine(t);
-
-                }
+			    dev.WriteByte(96);
+			    byte[] treeBytes = dev.ReadBytes(3);
 			}
 		}
 	}
